249ee33daf5adc2226948f3598c79b5a880d27ea
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-08-29 16:56:17 +0900

    Default.

---
+++ nyasocom_sun_app/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Webkit;
+using Android.Views;
+
+namespace nyasocom_sun_app
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        WebView web_view;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetWebViewClient(new NyasocomSunAppClient());
+            web_view.LoadUrl("https://blog.nyasocom.net");
+        }
+        public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && web_view.CanGoBack())
+            {
+                web_view.GoBack();
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    public class NyasocomSunAppClient : WebViewClient
+    {
+        [System.Obsolete]
+        public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        {
+            view.LoadUrl(url);
+            return false;
+        }
+
+    }
+}
